6273fb9f2ac16fa02bce87dbe9d5d6afd1030186
Jeff <js3600@gmail.com>
2025-03-13 23:54:48 -0400

    add initial spring boot initializer files

---
+++ todo/src/test/java/com/shen/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shen.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
